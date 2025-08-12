183faee4642504b80303d428aa0031b44dc28408
Dugald Morrow <dmorrow@atlassian.com>
2025-08-13 07:54:30 +1000

    Expand panel icon improvements

--- static/spa/src/widget/ExpandIcon.tsx
+++ static/spa/src/widget/ExpandIcon.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import FullscreenEnterIcon from '@atlaskit/icon/core/fullscreen-enter';
+import FullscreenExitIcon from '@atlaskit/icon/core/fullscreen-exit';
 
 export type ExpandIconProps = {
   isExpanded: boolean;
@@ -8,6 +10,22 @@ export type ExpandIconProps = {
 
 const ExpandIcon = (props: ExpandIconProps) => {
   const title = props.label || (props.isExpanded ? 'Collapse panel' : 'Expand panel');
+
+  const renderIcon = () => {
+    if (props.isExpanded) {
+      return (
+        <FullscreenExitIcon
+          label="Collapse panel"
+        />
+      );
+    } else {
+      return (
+        <FullscreenEnterIcon
+          label="Expand panel"
+        />
+      );
+    }
+  }
   
   return (
     <button
@@ -16,21 +34,10 @@ const ExpandIcon = (props: ExpandIconProps) => {
       title={title}
       aria-label={title}
     >
-      {props.isExpanded ? (
-        // Collapse icon (minimize/compress)
-        <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-          <path d="M19 13H5v-2h14v2z"/>
-          <path d="M19 17H5v-2h14v2z"/>
-          <path d="M5 9h14V7H5v2z"/>
-        </svg>
-      ) : (
-        // Expand icon (maximize/fullscreen)
-        <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-          <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>
-        </svg>
-      )}
+      {renderIcon()}
     </button>
   );
+
 };
 
 export default ExpandIcon;
